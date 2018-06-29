b9f7cfed35cdde9ef7259822287189430d199780
sri sai <31199874+srisai96@users.noreply.github.com>
2018-06-29 15:27:10 +0530

    day-5

---
+++ week-1/day-5/inflightEntertainment.java
@@ -0,0 +1,110 @@
+import java.util.Map;
+import java.util.HashMap;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static boolean canTwoMoviesFillFlight(int[] movieLengths, int flightLength) {
+
+        // determine if two movies add up to the flight length
+        if(movieLengths.length<=0)return false;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int first=movieLengths[0];
+        for(int i=0;i<movieLengths.length;i++){
+            if(map.containsKey(movieLengths[i]))
+            {
+                map.put(movieLengths[i],map.get(movieLengths[i])+1);
+            }
+            else map.put(movieLengths[i],1);
+        }
+
+
+        for(Map.Entry m:map.entrySet()){
+            int value = (int) m.getValue();
+            int key = (int) m.getKey();
+            int remaining = flightLength-key;
+            if (key==remaining && value>1) return true;
+            if(map.containsKey(remaining) && remaining!=key){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void shortFlightTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {2, 4}, 1);
+        assertFalse(result);
+    }
+
+    @Test
+    public void longFlightTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {2, 4}, 6);
+        assertTrue(result);
+    }
+
+    @Test
+    public void onlyOneMovieHalfFlightLenghtTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {3, 8}, 6);
+        assertFalse(result);
+    }
+
+    @Test
+    public void twoMoviesHalfFlightLengthTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {3, 8, 3}, 6);
+        assertTrue(result);
+    }
+
+    @Test
+    public void lotsOfPossiblePairsTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {1, 2, 3, 4, 5, 6}, 7);
+        assertTrue(result);
+    }
+
+    @Test
+    public void oneMovieTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {6}, 6);
+        assertFalse(result);
+    }
+
+    @Test
+    public void noMoviesTest() {
+        final boolean result = canTwoMoviesFillFlight(new int[] {}, 6);
+        assertFalse(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
